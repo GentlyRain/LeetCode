0ad627f44318c68c1d1b10dc44d08c3c680f5f22
GentlyRain <3294872564@qq.com>
2021-05-23 10:14:02 +0800

    迭代实现链表倒置

---
+++ NodeList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 链表
+    /// </summary>
+    public class NodeList
+    {
+        int val;
+        NodeList next;
+
+        public NodeList(int val, NodeList next)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+
+
+        /// <summary>
+        /// 迭代
+        /// </summary>
+        /// <returns></returns>
+        public static NodeList Recursion(NodeList head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            NodeList newNodeList = Recursion(head.next);
+            head.next.next = head;
+            head.next = null;
+
+            return newNodeList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /*** 链表倒置 ***/
+                NodeList node5 = new NodeList(5, null);
+                NodeList node4 = new NodeList(4, node5);
+                NodeList node3 = new NodeList(3, node4);
+                NodeList node2 = new NodeList(2, node3);
+                NodeList node1 = new NodeList(1, node2);
+
+                // 递归
+                NodeList newNodeList = NodeList.Recursion(node1);
+                Console.WriteLine($"{newNodeList}");
+                /*** 链表倒置 ***/
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            Console.ReadLine();
+        }
+    }
+}
